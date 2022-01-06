b8ae2ca35bc299052624d2ca0b75dcbd7c4038fc
Michael Sanabria <maicolsana12@gmail.com>
2022-01-06 17:49:54 -0500

    Fixing details to start exercises part2c

--- fso-react-app-part1/src/index.js
+++ fso-react-app-part1/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToDoList from './ToDoList';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToDoList />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
