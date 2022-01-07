01132f77622a87bd983db61d9ab29a52dfd81661
Michael Sanabria <maicolsana12@gmail.com>
2022-01-07 10:52:50 -0500

    Fixing ..

--- fso-react-app-part1/src/App.js
+++ fso-react-app-part1/src/App.js
@@ -2,55 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
-import Persons from './ComponentsFormPersons/Persons';
-import PersonsFiltered from './ComponentsFormPersons/PersonsFiltered';
-import PersonsForm from './ComponentsFormPersons/PersonsForm';
-
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState();
-  const [newFilter, setNewFilter] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
-      .then((response) => setPersons(response.data));
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => setCountries(response.data));
   }, []);
 
-  console.log(persons);
-
-  const addPeople = (e) => {
-    e.preventDefault();
-    window.alert(
-      `${newName} with number ${newNumber} has been added to the PhoneBook`
-    );
-    setPersons([...persons, { name: newName, number: newNumber }]);
-  };
-
-  const handleNameInput = (e) => {
-    setNewName(e.target.value);
-  };
-
-  const handleNumberInput = (e) => {
-    setNewNumber(e.target.value);
-  };
+  console.log(countries[0]);
 
   const handleFilterInput = (e) => {
-    setNewFilter(e.target.value);
+    setFilter(e.target.value);
   };
-
-  const personsFilter = persons.filter((p) => p.name === newFilter);
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Find Countries! </h2>
       <p>
-        filter shown: <input onChange={handleFilterInput} />
+        Find Countries: <input onChange={handleFilterInput} />
       </p>
-      <PersonsForm props={[addPeople, handleNameInput, handleNumberInput]} />
-      <Persons persons={persons} />
-      <PersonsFiltered personsFilter={personsFilter} />
     </div>
   );
 };
