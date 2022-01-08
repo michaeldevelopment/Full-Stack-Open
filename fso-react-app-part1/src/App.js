49a25c052d25b95fbb1fb424ad66309ef20b3f55
Michael Sanabria <maicolsana12@gmail.com>
2022-01-08 11:37:54 -0500

    Modified

--- fso-react-app-part1/src/App.js
+++ fso-react-app-part1/src/App.js
@@ -2,19 +2,43 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
+import Persons from './ComponentsFormPersons/Persons';
+import PersonsFiltered from './ComponentsFormPersons/PersonsFiltered';
+import PersonsForm from './ComponentsFormPersons/PersonsForm';
+
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [filter, setNewFilter] = useState('');
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState();
+  const [newFilter, setNewFilter] = useState('');
 
   useEffect(() => {
     axios
-      .get('https://restcountries.com/v3.1/all')
-      .then((response) => setCountries(response.data));
+      .get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data));
   }, []);
+
+  const addPeople = (e) => {
+    e.preventDefault();
+    window.alert(
+      `${newName} with number ${newNumber} has been added to the PhoneBook`
+    );
+    setPersons([...persons, { name: newName, number: newNumber }]);
+  };
+
+  const handleNameInput = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberInput = (e) => {
+    setNewNumber(e.target.value);
+  };
 
   const handleFilterInput = (e) => {
     setNewFilter(e.target.value);
   };
+
+  const personsFilter = persons.filter((p) => p.name === newFilter);
 
   return (
     <div>
@@ -22,7 +46,9 @@ const App = () => {
       <p>
         filter shown: <input onChange={handleFilterInput} />
       </p>
-      {filter}
+      <PersonsForm props={[addPeople, handleNameInput, handleNumberInput]} />
+      <Persons persons={persons} />
+      <PersonsFiltered personsFilter={personsFilter} />
     </div>
   );
 };
